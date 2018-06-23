163eb70d591aa3968aeb8c5bfdfee686422b8dc6
John Doody <40151857+JDoody@users.noreply.github.com>
2018-06-23 18:56:34 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -5,14 +5,19 @@
 ## User should call this function by creating a invertible matrix using makeCacheMatrix and storing in a variable of their choice
 ## example my_matrix <- makeCacheMatrix(rnorm(16,4,4))
 
-makeCacheMatrix <- function(inverted_matrix = matrix()){ 
-list(
-  invert = function(users_matrix){
-  inverted_matrix <<- solve(users_matrix)
-  })
+makeCacheMatrix <- function(x = matrix()){ 
+
+          m <- NULL
+          set <- function(y) {
+          x <<- y
+          m <<- NULL
+          }
+          get <- function() x
+          setcache <- function(solve) m <<- solve
+          getcache <- function() m
+          list(set = set, get = get,
+               setcache = setcache, getcache = getcache)
   }
-
-
 
 
 ## this function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
@@ -20,5 +25,13 @@ list(
 ## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getcache()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setcache(m)
+        m
 }
